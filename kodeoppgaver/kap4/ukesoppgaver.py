6c5f6531ed3b9e25c071b281cd67e176f3fd271a
Børge Bjørnstadjordet <borgetull@gmail.com>
2024-10-01 17:05:38 +0200

    4.5 4a

--- kodeoppgaver/kap4/ukesoppgaver.py
+++ kodeoppgaver/kap4/ukesoppgaver.py
@@ -2,13 +2,25 @@ from numpy import *
 
 '''
 Seksjon 4.5
-Oppgave 4
+Oppgave 3a
 '''
 
-A = matrix([[1, -2, 3, -1],
+A3 = matrix([[1, -2, 3, -1],
            [2, 3, -1, 3],
            [0, -1, 2, -2],
            [-2, 2, -1, 3]])
 
-Ainv = linalg.inv(A)
+A3inv = linalg.inv(A3)
 
+'''
+Seksjon  4.5
+Oppgave 4a
+'''
+
+A4 = matrix([[2, -1, 3],
+             [0, -1, 2],
+             [-4, 3, 1]])
+
+b4 = matrix([[-1], [2], [3]])
+
+x = linalg.solve(A4, b4)
